fd76a9d4eafdf74d11cf7bc3388d5a2980b48ca4
Siyabonga Samson Lukhele <samsonlukhele76@gmail.com>
2024-08-06 14:56:21 +0200

    Create header and make it responsive

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const showSidebar = () => {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex";
+};
+
+const hideSidebar = () => {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "none";
+};
