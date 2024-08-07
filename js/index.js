e84987cc8613b1189e9e736a2a515dca08dfc6c4
Siyabonga Samson Lukhele <samsonlukhele76@gmail.com>
2024-08-07 14:45:23 +0200

    Make months array

--- js/index.js
+++ js/index.js
@@ -7,3 +7,23 @@ const hideSidebar = () => {
   const sidebar = document.querySelector(".sidebar");
   sidebar.classList.remove("show");
 };
+
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const currentDate = new Date();
+const month = monthNames[currentDate.getUTCMonth()];
+const year = currentDate.getFullYear();
+document.getElementById("current-date").innerText = `${month} ${year}`;
