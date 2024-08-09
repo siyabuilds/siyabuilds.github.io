6b151e781cffefa709336e008b208a89f2c6a861
Siyabonga Samson Lukhele <samsonlukhele76@gmail.com>
2024-08-09 14:22:09 +0200

    Use sweetalert function in project

--- js/index.js
+++ js/index.js
@@ -27,3 +27,15 @@ const currentDate = new Date();
 const month = monthNames[currentDate.getUTCMonth()];
 const year = currentDate.getFullYear();
 document.getElementById("current-date").innerText = `${month} ${year}`;
+
+function showAlert() {
+  Swal.fire({
+    title: "Projects section not built yet",
+    text: "I haven't finished all of my projects as of yet, but you can view the work in progress on my GitHub dashboard.",
+    icon: "info",
+    confirmButtonText: "OK",
+    footer:
+      '<a href="https://github.com/markuptitan" target="_blank"><i class="fa-solid fa-arrow-up-right-from-square"></i> View my GitHub Dashboard</a>',
+  });
+  return false;
+}
